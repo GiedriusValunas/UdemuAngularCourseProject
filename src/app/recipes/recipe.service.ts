3e297c3183e365ffdb873fdf78c999d7d044d76c
Giedrius <giedrius1258@gmail.com>
2021-09-16 22:49:40 +0300

    Initial commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,51 @@
+import {Injectable} from '@angular/core';
+import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+  private recipes: Recipe [] = [
+    new Recipe('A test recipe',
+      'Just a test recipe',
+      'https://www.recipetineats.com/wp-content/uploads/2021/08/Spaghetti-Puttanesca_64-SQ.jpg?w=900',
+      [
+        new Ingredient('Meat', 3),
+        new Ingredient('Bread', 1)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.getRecipes());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.getRecipes());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.getRecipes());
+  }
+}
