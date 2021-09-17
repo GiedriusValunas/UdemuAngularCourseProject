2db173fc42c1b9ac888a9c8d3292aa2288a51d48
GiedriusValunas <valunas.giedrius@gmaiul.com>
2021-09-17 15:56:35 +0300

    Added logging service.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "./shopping-list.service";
 import {Subscription} from "rxjs";
+import {LoggingService} from "../logging.service";
 
 @Component({
   selector: 'app-shopping-list',
@@ -12,10 +13,12 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService) {
+  constructor(private slService: ShoppingListService,
+              private loggingService: LoggingService) {
   }
 
   ngOnInit(): void {
+    this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit');
     this.ingredients = this.slService.getIngredients();
     this.subscription = this.slService.ingredientsChanged.subscribe((ingredients) => {
       this.ingredients = ingredients
